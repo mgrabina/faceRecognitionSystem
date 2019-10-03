642d6282542f7c3ff7e6da38f68a1fe7d913b85f
Martin <martinnagelberg@gmail.com>
2019-10-03 03:55:42 -0300

    config

--- library/main.py
+++ library/main.py
@@ -1,9 +1,17 @@
 from library.PCA import PCA
 from library.KPCA import KPCA
+import configparser
 
 def main():
-    #PCA.test()
-    KPCA.test()
+
+	config = configparser.ConfigParser()
+	config.read('config.ini')
+	method = parser.get('ACTION', 'METHOD')
+	
+    # pca = PCA()
+    # pca.test()
+    #kpca = KPCA()
+    #kpca.test()
 
 
 if __name__ == "__main__":
