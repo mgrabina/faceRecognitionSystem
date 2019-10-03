d5b0192daf364dcc79dc1c49c4b13fc20d5683ed
Martin Grabina <mgrabina@itba.edu.ar>
2019-10-03 04:44:18 -0300

    Some running configs

--- library/PCA.py
+++ library/PCA.py
@@ -10,99 +10,121 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
 from methods import *
-
+from scipy import ndimage as im
 
 class PCA(object):
 
+    trained = False
+
     @staticmethod
-    def train(type):
+    def train(type, data):
+        if PCA.trained == False:
+            PCA.trained = True
+            mypath = '../att_faces/'
+            onlydirs = [f for f in listdir(mypath) if isdir(join(mypath, f))]
+
+            # image size
+            horsize = 92
+            versize = 112
+            areasize = horsize * versize
+
+            # number of figures
+            personno = 40
+            trnperper = 6
+            tstperper = 4
+            trnno = personno * trnperper
+            tstno = personno * tstperper
+
+            # TRAINING SET
+            images = np.zeros([trnno, areasize])
+            person = np.zeros([trnno, 1])
+            imno = 0
+            per = 0
+            for dire in onlydirs:
+                for k in range(1, trnperper + 1):
+                    a = plt.imread(mypath + dire + '/{}'.format(k) + '.pgm') / 255.0
+                    images[imno, :] = np.reshape(a, [1, areasize])
+                    person[imno, 0] = per
+                    imno += 1
+                per += 1
+
+            # TEST SET
+            imagetst = np.zeros([tstno, areasize])
+            persontst = np.zeros([tstno, 1])
+            imno = 0
+            per = 0
+            for dire in onlydirs:
+                for k in range(trnperper, 10):
+                    a = plt.imread(mypath + dire + '/{}'.format(k) + '.pgm') / 255.0
+                    imagetst[imno, :] = np.reshape(a, [1, areasize])
+                    persontst[imno, 0] = per
+                    imno += 1
+                per += 1
+
+            # CARA MEDIA
+            meanimage = np.mean(images, 0)
+            fig, axes = plt.subplots(1, 1)
+            axes.imshow(np.reshape(meanimage, [versize, horsize]) * 255, cmap='gray')
+            fig.suptitle('Imagen media')
+
+            # resto la media
+            images = [images[k, :] - meanimage for k in range(images.shape[0])]
+            imagetst = [imagetst[k, :] - meanimage for k in range(imagetst.shape[0])]
+
+            # PCA
+            images_matrix = np.asmatrix(images)
+            S, V = getSingluarValuesAndEigenVectors(images_matrix)
+
+            nmax = V.shape[0]
+            nmax = 100
+            accs = np.zeros([nmax, 1])
+
         if type == 'test':
-            print "Testing"
+            print "Testing..."
+            for neigen in range(1, nmax):
+                # Me quedo sólo con las primeras autocaras
+                B = V[0:neigen, :]
+                # proyecto
+                improy = np.dot(images, np.transpose(B))
+                imtstproy = np.dot(imagetst, np.transpose(B))
+
+                # SVM
+                # entreno
+                clf = svm.LinearSVC()
+                clf.fit(improy, person.ravel())
+                accs[neigen] = clf.score(imtstproy, persontst.ravel())
+                print('Precisión con {0} autocaras: {1} %\n'.format(neigen, accs[neigen] * 100))
+
+            fig, axes = plt.subplots(1, 1)
+            axes.semilogy(range(nmax), (1 - accs) * 100)
+            axes.set_xlabel('No. autocaras')
+            axes.grid(which='Both')
+            fig.suptitle('Error')
+
         elif type == 'predict':
             print "Predicting"
+            picture = im.imread(data['path'])
+            fig, axes = plt.subplots(1, 1)
+            axes.imshow(picture, cmap='gray')
+            fig.suptitle('Image to predict')
+            plt.show()
+            picture = np.reshape((picture - 127.5) / 127.5, [1, data['h_size'] * data['v_size']])
 
-        mypath = '../att_faces/'
-        onlydirs = [f for f in listdir(mypath) if isdir(join(mypath, f))]
-
-        # image size
-        horsize = 92
-        versize = 112
-        areasize = horsize * versize
-
-        # number of figures
-        personno = 40
-        trnperper = 6
-        tstperper = 4
-        trnno = personno * trnperper
-        tstno = personno * tstperper
-
-        # TRAINING SET
-        images = np.zeros([trnno, areasize])
-        person = np.zeros([trnno, 1])
-        imno = 0
-        per = 0
-        for dire in onlydirs:
-            for k in range(1, trnperper + 1):
-                a = plt.imread(mypath + dire + '/{}'.format(k) + '.pgm') / 255.0
-                images[imno, :] = np.reshape(a, [1, areasize])
-                person[imno, 0] = per
-                imno += 1
-            per += 1
-
-        # TEST SET
-        imagetst = np.zeros([tstno, areasize])
-        persontst = np.zeros([tstno, 1])
-        imno = 0
-        per = 0
-        for dire in onlydirs:
-            for k in range(trnperper, 10):
-                a = plt.imread(mypath + dire + '/{}'.format(k) + '.pgm') / 255.0
-                imagetst[imno, :] = np.reshape(a, [1, areasize])
-                persontst[imno, 0] = per
-                imno += 1
-            per += 1
-
-        # CARA MEDIA
-        meanimage = np.mean(images, 0)
-        fig, axes = plt.subplots(1, 1)
-        axes.imshow(np.reshape(meanimage, [versize, horsize]) * 255, cmap='gray')
-        fig.suptitle('Imagen media')
-
-        # resto la media
-        images = [images[k, :] - meanimage for k in range(images.shape[0])]
-        imagetst = [imagetst[k, :] - meanimage for k in range(imagetst.shape[0])]
-
-        # PCA
-        images_matrix = np.asmatrix(images)
-        S, V = getSingluarValuesAndEigenVectors(images_matrix)
-
-        nmax = V.shape[0]
-        nmax = 100
-        accs = np.zeros([nmax, 1])
-        for neigen in range(1, nmax):
-            # Me quedo sólo con las primeras autocaras
-            B = V[0:neigen, :]
-            # proyecto
+            B = V[0:60, :]
             improy = np.dot(images, np.transpose(B))
-            imtstproy = np.dot(imagetst, np.transpose(B))
-
-            # SVM
-            # entreno
             clf = svm.LinearSVC()
             clf.fit(improy, person.ravel())
-            accs[neigen] = clf.score(imtstproy, persontst.ravel())
-            print('Precisión con {0} autocaras: {1} %\n'.format(neigen, accs[neigen] * 100))
-
-        fig, axes = plt.subplots(1, 1)
-        axes.semilogy(range(nmax), (1 - accs) * 100)
-        axes.set_xlabel('No. autocaras')
-        axes.grid(which='Both')
-        fig.suptitle('Error')
+            picture -= meanimage
+            pictureProy = np.dot(picture, B.T)
+            print("Subject is: {} \n".format(clf.predict(pictureProy)[0]))
+        else:
+            print "Error"
 
     @staticmethod
     def test():
-        PCA.train('test')
+        PCA.train('test', {})
 
     @staticmethod
-    def predict(image):
-        PCA.train('predict')
+    def predict(path, data):
+        PCA.train('predict', data)
+
--- library/main.py
+++ library/main.py
@@ -3,15 +3,20 @@ from library.KPCA import KPCA
 import configparser
 
 def main():
-
-	config = configparser.ConfigParser()
-	config.read('config.ini')
-	method = config.get('ACTION', 'METHOD')
-	
-    # pca = PCA()
-    # pca.test()
-    #kpca = KPCA()
-    #kpca.test()
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    method = config.get('ACTION', 'METHOD')
+    path = config.get('ACTION', 'PATH')
+    faces = config.getint('ACTION', 'FACES')
+    v_size = config.getint('SIZES', 'V_SIZE')
+    h_size = config.getint('SIZES', 'H_SIZE')
+    # PCA.test()
+    PCA.predict(path, data={
+        'faces': faces,
+        'path': path,
+        'v_size': v_size,
+        'h_size': h_size
+    })
 
 
 if __name__ == "__main__":
